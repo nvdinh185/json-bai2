6430f296d393ffcfc0b7c732036cb4b018388af6
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-05 08:34:21 +0700

    x

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bookRoute from "./routes/bookRoute.js";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import bookRoute from "./routes/bookRoute.js";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 
+const PORT = process.env.PORT;
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const publicPath = path.join(__dirname, "client");
+app.use(express.static(publicPath));
+
 app.get("/", (req, res, next) => {
-  res.send("THIS IS SERVER");
+  res.sendFile(path.join(publicPath, 'list.html'));
 });
 
 const connect = async () => {
